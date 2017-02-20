2a430375dab13ab7b3072517e81056705cefd740
ih <irvin.hwang@gmail.com>
2017-02-20 14:19:55 -0500

    add label to keyboard key component

--- components/keyboard-key.js
+++ components/keyboard-key.js
@@ -4,9 +4,21 @@ AFRAME.registerComponent('keyboard-key', {
   },
 
   init: function () {
-    var _this = this;
-    this.el.addEventListener('pressed', function () {
+    let _this = this;
+    this.el.addEventListener('pressed', () => {
       console.log(_this.data.value);
     });
+
+    let uiButtonAttributes = this.el.getAttribute('ui-button');
+
+    let label = document.createElement('a-entity');
+    label.setAttribute('text', {
+      value: this.data.value,
+      align: 'center',
+      width: 3
+    });
+    label.setAttribute('rotation', { x: -90, y: 0, z: 0 });
+    label.setAttribute('position', {x: 0, y: uiButtonAttributes.size, z: 0});
+    this.el.appendChild(label);
   }
 })

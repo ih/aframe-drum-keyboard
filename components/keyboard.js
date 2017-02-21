e24944f883776a43e2036740a5379e11517a8ba8
ih <irvin.hwang@gmail.com>
2017-02-21 15:29:03 -0500

    add more keys to keyboard and adjust layout

--- components/keyboard.js
+++ components/keyboard.js
@@ -1,11 +1,26 @@
 AFRAME.registerComponent('keyboard', {
 
   init: function () {
-    let defaultKeysData = [{value: 'q'}, {value: 'w'}, {value: '\n', label: 'enter'}];
+    let defaultKeysData = this.generateDefaultKeyData();
     let keys = this.constructDefaultKeys(defaultKeysData);
     let layout = this.constructDefaultLayout(defaultKeysData);
     this.addKeysToKeyboard(defaultKeysData, keys, layout);
     // connectKeyboardToDisplay();
+  },
+
+  generateDefaultKeyData: function () {
+    return '1234567890qwertyuiopasdfghjkl\nzxcvbnm '.split('').map((character) => {
+      var keyData = {value: character};
+      switch (character) {
+        case '\n':
+        keyData.label = 'enter';
+        break;
+        case ' ':
+        keyData.label = 'space';
+        break;
+      }
+      return keyData;
+    });
   },
 
   constructDefaultKeys: function (keysData) {
@@ -23,13 +38,26 @@ AFRAME.registerComponent('keyboard', {
   },
 
   constructDefaultLayout: function (keysData) {
+    let row1 = keysData.slice(0, 10);
+    let row2 = keysData.slice(10, 20);
+    let row3 = keysData.slice(20, 30);
+    let row4 = keysData.slice(30, 37);
+    let space = keysData[37];
     let layout = {};
+    let height = -1;
     let keySpacing = .3;
-    let currentX = 0;
-    keysData.forEach((keyData) => {
-      layout[keyData.value] = {x: currentX, y: 0, z: 0};
-      currentX += keySpacing;
+    let rowSpacing = .5;
+    let currentZ = 0;
+    let leftRowPadding = .3;
+    [row1, row2, row3].forEach((row, index) => {
+      let currentX = -1.5 + (index * leftRowPadding);
+      row.forEach((keyData) => {
+        layout[keyData.value] = {x: currentX, y: height, z: currentZ};
+        currentX += keySpacing;
+      });
+      currentZ += rowSpacing;
     });
+    layout[space.value] = {x: 0, y: height, z: currentZ};
     return layout;
   },
 
